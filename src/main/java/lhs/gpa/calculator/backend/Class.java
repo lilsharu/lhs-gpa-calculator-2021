c50c1d16875030b460b6f879a6dc316087f80edc
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-08 16:30:19 -0400

    Added More Classes and Reorganized

--- src/main/java/lhs/gpa/calculator/backend/Class.java
+++ src/main/java/lhs/gpa/calculator/backend/Class.java
@@ -4,17 +4,19 @@ public class Class {
 
     private String name;
     private double credits;
+    /* Class Levels */
     public static final double AP = 5.0;
     private String department;
     private String length = FULL_YEAR;
-    private int courseNumber;
-
-    public static final String FULL_YEAR = "Full Year";
-    public static final String HALF_YEAR = "Half Year";
     public static final double HONORS = 4.5;
     public static final double CP = 4.0;
-
+    /* Class Lengths */
+    public static final String FULL_YEAR = "Full Year";
+    /* Class Departments */
     public static final String BUSINESS_DEPARTMENT = "Business";
+    private double level;
+    public static final String HALF_YEAR = "Half Year";
+    private int classNumber;
     public static final String ENGLISH_DEPARTMENT = "English";
     public static final String FCS_DEPARTMENT = "Family Consumer Science";
     public static final String PE_DEPARTMENT = "Health and Physical Education";
@@ -26,167 +28,21 @@ public class Class {
     public static final String VISUAL_ART_DEPARTMENT = "Visual Arts";
     public static final String WORLD_LANGUAGE_DEPARTMENT = "World Language";
 
-    /*
-     * =============================================================
-     * =================== BUSINESS DEPARTMENT =====================
-     * =============================================================
-     */
-    public static final Class INTRODUCTION_TO_BUSINESS = new Class("INTRODUCTION TO BUSINESS", 5, CP, BUSINESS_DEPARTMENT, 613);
-    public static final Class COMPUTER_APPLICATIONS = new Class("COMPUTER APPLICATIONS", 5, CP, BUSINESS_DEPARTMENT, 623);
-    public static final Class CAREER_EXPLORATION = new Class("CAREER EXPLORATION", 5, CP, BUSINESS_DEPARTMENT, 771);
-    public static final Class CAREER_INTERNSHIP_PROGRAM_1 = new Class("CAREER INTERNSHIP PROGRAM 1", 5, CP, BUSINESS_DEPARTMENT, 772);
-    public static final Class CAREER_INTERNSHIP_PROGRAM_2 = new Class("CAREER INTERNSHIP PROGRAM 2", 5, CP, BUSINESS_DEPARTMENT, 773);
-    public static final Class COMMUNITY_BASED_INSTRUCTION = new Class("COMMUNITY BASED INSTRUCTION", 5, CP, BUSINESS_DEPARTMENT, 6);
-    public static final Class ACCOUNTING_1 = new Class("ACCOUNTING 1", 5, CP, BUSINESS_DEPARTMENT, 621);
-    public static final Class ACCOUNTING_2 = new Class("ACCOUNTING 2", 5, CP, BUSINESS_DEPARTMENT, 632);
-    public static final Class COOPERATIVE_WORK_EXPERIENCE = new Class("COOPERATIVE WORK EXPERIENCE", 15, CP, BUSINESS_DEPARTMENT, 642);
-    public static final Class BUSINESS_LAW_1 = new Class("BUSINESS LAW 1", 5, CP, BUSINESS_DEPARTMENT, 635);
-    public static final Class BUSINESS_LAW_2 = new Class("BUSINESS LAW 2", 5, CP, BUSINESS_DEPARTMENT, 638);
-    public static final Class ENTREPRENEURSHIP = new Class("ENTREPRENEURSHIP", 5, CP, BUSINESS_DEPARTMENT, 636);
-    public static final Class MARKETING = new Class("MARKETING", 5, CP, BUSINESS_DEPARTMENT, 634);
-    public static final Class MICROSOFT_OFFICE_SPECIALIST = new Class("MICROSOFT OFFICE SPECIALIST", 5, CP, BUSINESS_DEPARTMENT, 625);
-    public static final Class GLOBAL_BUSINESS = new Class("GLOBAL BUSINESS", 5, CP, BUSINESS_DEPARTMENT, 640);
-    public static final Class AP_ECONOMICS = new Class("AP ECONOMICS", 5, AP, BUSINESS_DEPARTMENT, 650);
-    public static final Class ECONOMICS = new Class("ECONOMICS", 2.5, CP, BUSINESS_DEPARTMENT, 633, HALF_YEAR);
-    public static final Class INVESTING
-            = new Class("INVESTING", 2.5, CP, BUSINESS_DEPARTMENT, 614, HALF_YEAR);
-    public static final Class PERSONAL_FINANCIAL_LITERACY
-            = new Class("PERSONAL FINANCIAL LITERACY", 2.5, CP, BUSINESS_DEPARTMENT, 626, HALF_YEAR);
-    public static final Class PERSONAL_FINANCIAL_LITERACY_HYBRID
-            = new Class("PERSONAL FINANCIAL LITERACY HYBRID", 2.5, CP, BUSINESS_DEPARTMENT, 626, HALF_YEAR);
-    public static final Class SPORTS_AND_ENTERTAINMENT_MARKETING
-            = new Class("SPORTS AND ENTERTAINMENT MARKETING", 2.5, CP, BUSINESS_DEPARTMENT, 615, HALF_YEAR);
-    public static final Class SOCIAL_MEDIA_MARKETING
-            = new Class("SOCIAL MEDIA MARKETING", 2.5, CP, BUSINESS_DEPARTMENT, 616, HALF_YEAR);
-    public static final Class WEB_DESIGN
-            = new Class("WEB DESIGN", 2.5, CP, BUSINESS_DEPARTMENT, 627, HALF_YEAR);
-    public static final Class ENGLISH_FOR_SUCCESS_1
-            = new Class("ENGLISH FOR SUCCESS 1", 5, CP, ENGLISH_DEPARTMENT, 116);
-    public static final Class ENGLISH_FOR_SUCCESS_2
-            = new Class("ENGLISH FOR SUCCESS 2", 5, CP, ENGLISH_DEPARTMENT, 117);
-    public static final Class GENERAL_ENGLISH_1
-            = new Class("GENERAL ENGLISH 1", 5, CP, ENGLISH_DEPARTMENT, 171);
-
-    /*
-     * =============================================================
-     * ==================== ENGLISH DEPARTMENT =====================
-     * =============================================================
-     */
-    public static final Class ENGLISH_1_CP
-            = new Class("ENGLISH 1 CP", 5, CP, ENGLISH_DEPARTMENT, 112);
-    public static final Class ENGLISH_1_HUMANITIES_CP
-            = new Class("ENGLISH 1 HUMANITIES CP", 5, CP, ENGLISH_DEPARTMENT, 112);
-    public static final Class ENGLISH_1_HONORS
-            = new Class("ENGLISH 1 HONORS", 5, HONORS, ENGLISH_DEPARTMENT, 110);
-    public static final Class ENGLISH_1_HUMANITIES_HONORS
-            = new Class("ENGLISH 1 HUMANITIES HONORS", 5, HONORS, ENGLISH_DEPARTMENT, 110);
-    public static final Class GENERAL_ENGLISH_2
-            = new Class("GENERAL ENGLISH 2", 5, CP, ENGLISH_DEPARTMENT, 172);
-    public static final Class ENGLISH_2_CP
-            = new Class("ENGLISH 2 CP", 5, CP, ENGLISH_DEPARTMENT, 122);
-    public static final Class ENGLISH_2_HUMANITIES_CP
-            = new Class("ENGLISH 2 HUMANITIES CP", 5, CP, ENGLISH_DEPARTMENT, 122);
-    public static final Class ENGLISH_2_HONORS
-            = new Class("ENGLISH 2 HONORS", 5, HONORS, ENGLISH_DEPARTMENT, 120);
-    public static final Class ENGLISH_2_HUMANITIES_HONORS
-            = new Class("ENGLISH 2 HUMANITIES HONORS", 5, HONORS, ENGLISH_DEPARTMENT, 120);
-    public static final Class GENERAL_ENGLISH_3
-            = new Class("GENERAL ENGLISH 3", 5, CP, ENGLISH_DEPARTMENT, 173);
-    public static final Class ENGLISH_3_CP
-            = new Class("ENGLISH 3 CP", 5, CP, ENGLISH_DEPARTMENT, 132);
-    public static final Class ENGLISH_3_HUMANITIES_CP
-            = new Class("ENGLISH 3 HUMANITIES CP", 5, CP, ENGLISH_DEPARTMENT, 132);
-    public static final Class ENGLISH_3_HONORS
-            = new Class("ENGLISH 3 HONORS", 5, HONORS, ENGLISH_DEPARTMENT, 130);
-    public static final Class ENGLISH_3_HUMANITIES_HONORS
-            = new Class("ENGLISH 3 HUMANITIES HONORS", 5, HONORS, ENGLISH_DEPARTMENT, 130);
-    public static final Class AP_ENGLISH_LANGUAGE_AND_COMPOSITION
-            = new Class("AP ENGLISH LANGUAGE AND COMPOSITION", 5, AP, ENGLISH_DEPARTMENT, 131);
-    public static final Class GENERAL_ENGLISH_4
-            = new Class("GENERAL ENGLISH 4", 5, CP, ENGLISH_DEPARTMENT, 174);
-    public static final Class ENGLISH_4_CP
-            = new Class("ENGLISH 4 CP", 5, CP, ENGLISH_DEPARTMENT, 142);
-    public static final Class ENGLISH_4_HONORS
-            = new Class("ENGLISH 4 HONORS", 5, HONORS, ENGLISH_DEPARTMENT, 140);
-    public static final Class AP_ENGLISH_LITERATURE_AND_COMPOSITION
-            = new Class("AP ENGLISH LITERATURE AND COMPOSITION", 5, AP, ENGLISH_DEPARTMENT, 141);
-    public static final Class CREATIVE_WRITING_1
-            = new Class("CREATIVE WRITING 1", 5, CP, ENGLISH_DEPARTMENT, 118);
-    public static final Class CREATIVE_WRITING_2
-            = new Class("CREATIVE WRITING 2", 5, CP, ENGLISH_DEPARTMENT, 129);
-    public static final Class CREATIVE_WRITING_3
-            = new Class("CREATIVE WRITING 3", 5, CP, ENGLISH_DEPARTMENT, 149);
-    public static final Class JOURNALISM
-            = new Class("JOURNALISM", 5, CP, ENGLISH_DEPARTMENT, 125);
-    public static final Class PUBLIC_SPEAKING_AND_DEBATE
-            = new Class("PUBLIC SPEAKING AND DEBATE", 5, CP, ENGLISH_DEPARTMENT, 135);
-    public static final Class READING_AND_WRITING_FOR_SUCCESS_1
-            = new Class("READING AND WRITING FOR SUCCESS 1", 5, CP, ENGLISH_DEPARTMENT, 180);
-    public static final Class READING_AND_WRITING_FOR_SUCCESS_2
-            = new Class("READING AND WRITING FOR SUCCESS 2", 5, CP, ENGLISH_DEPARTMENT, 181);
-    public static final Class READING_AND_WRITING_FOR_SUCCESS_3
-            = new Class("READING AND WRITING FOR SUCCESS 3", 5, CP, ENGLISH_DEPARTMENT, 182);
-    public static final Class READING_AND_WRITING_FOR_SUCCESS_4
-            = new Class("READING AND WRITING FOR SUCCESS 4", 5, CP, ENGLISH_DEPARTMENT, 184);
-    public static final Class SPEECH_AND_DRAMA
-            = new Class("SPEECH AND DRAMA", 5, CP, ENGLISH_DEPARTMENT, 136);
-    public static final Class AP_SEMINAR_HUMANITIES
-            = new Class("AP SEMINAR: HUMANITIES", 5, AP, ENGLISH_DEPARTMENT, 234);
-    public static final Class AP_RESEARCH_HUMANITIES
-            = new Class("AP RESEARCH: HUMANITIES", 5, AP, ENGLISH_DEPARTMENT, 235);
-    public static final Class WORLD_FILM
-            = new Class("WORLD FILM", 5, CP, ENGLISH_DEPARTMENT, 138);
-    public static final Class YEARBOOK
-            = new Class("YEARBOOK", 5, CP, ENGLISH_DEPARTMENT, 155);
-    public static final Class NEWSPAPER
-            = new Class("NEWSPAPER", 5, CP, ENGLISH_DEPARTMENT, 157);
-    public static final Class ENGLISH_ENRICHMENT_12
-            = new Class("ENGLISH ENRICHMENT 12", 5, CP, ENGLISH_DEPARTMENT, 103);
-    public static final Class MEDIA_LITERACY
-            = new Class("MEDIA LITERACY", 2.5, CP, ENGLISH_DEPARTMENT, 134, HALF_YEAR);
-    public static final Class SHAKESPEARE_AND_DRAMA
-            = new Class("SHAKESPEARE AND DRAMA", 2.5, CP, ENGLISH_DEPARTMENT, 139, HALF_YEAR);
-    public static final Class FASHION_1
-            = new Class("FASHION 1", 5, CP, FCS_DEPARTMENT, 711);
-    public static final Class FASHION_2
-            = new Class("FASHION 2", 5, CP, FCS_DEPARTMENT, 721);
-    public static final Class FOODS_AND_NUTRITION
-            = new Class("FOODS AND NUTRITION", 5, CP, FCS_DEPARTMENT, 722);
-
-    /*
-     * =============================================================
-     * ====================== FCS DEPARTMENT =======================
-     * =============================================================
-     */
-    public static final Class SENIOR_SERVICE
-            = new Class("SENIOR SERVICE", 5, CP, FCS_DEPARTMENT, 658);
-    public static final Class INTERIOR_DESIGN
-            = new Class("INTERIOR DESIGN", 2.5, CP, FCS_DEPARTMENT, 712);
-    public static final Class HOTEL_AND_RESTAURANT_MANAGEMENT
-            = new Class("HOTEL AND RESTAURANT MANAGEMENT", 2.5, CP, FCS_DEPARTMENT, 720);
-    public static final Class FAMILY_AND_CHILD_DEVELOPMENT_1
-            = new Class("FAMILY AND CHILD DEVELOPMENT 1", 2.5, CP, FCS_DEPARTMENT, 730);
-    public static final Class FAMILY_AND_CHILD_DEVELOPMENT_2
-            = new Class("FAMILY AND CHILD DEVELOPMENT 2", 2.5, CP, FCS_DEPARTMENT, 737);
-    public static final Class INDEPENDENT_STUDY_IN_TECHNOLOGY
-            = new Class("INDEPENDENT STUDY IN TECHNOLOGY", 2.5, CP, FCS_DEPARTMENT, 746, HALF_YEAR);
-    private double level;
-
-    public Class(String name, double credits, double level, String department, int courseNumber) {
+    public Class(String name, double credits, double level, String department, int classNumber) {
         this.name = name;
         this.credits = round(credits);
         this.level = level;
         this.department = department;
-        this.courseNumber = courseNumber;
+        this.classNumber = classNumber;
     }
 
-    public Class(String name, double credits, double level, String department, int courseNumber, String length) {
+    public Class(String name, double credits, double level, String department, int classNumber, String length) {
         this.name = name;
         this.credits = round(credits);
         this.level = level;
         this.department = department;
         this.length = length;
-        this.courseNumber = courseNumber;
+        this.classNumber = classNumber;
     }
 
     public String getName() {
@@ -229,12 +85,12 @@ public class Class {
         this.length = length;
     }
 
-    public int getCourseNumber() {
-        return courseNumber;
+    public int getClassNumber() {
+        return classNumber;
     }
 
-    public void setCourseNumber(int courseNumber) {
-        this.courseNumber = courseNumber;
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
     }
 
     /* Other Useful Methods */

e3a48434dbc40547dd52ebd8065aa66379df01a2
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-11 21:27:50 -0400

    Created a ClassNumber class and adjusted Class to Follow

---
+++ src/main/java/lhs/gpa/calculator/backend/ClassNumber.java
@@ -0,0 +1,50 @@
+package lhs.gpa.calculator.backend;
+
+public class ClassNumber {
+    private final int    number;
+    private       String addition = "";
+    
+    public ClassNumber(int number) {
+        this.number = number;
+    }
+    
+    public ClassNumber(int number, String addition) {
+        this.number   = number;
+        this.addition = addition;
+    }
+    
+    public ClassNumber(String courseNumber) {
+        this.number = Integer.parseInt(courseNumber.substring(0, 3));
+        if (courseNumber.length() > 3) {
+            addition = courseNumber.substring(3);
+        }
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public String getAddition() {
+        return addition;
+    }
+    
+    public void setAddition(String addition) {
+        this.addition = addition;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        ClassNumber that = (ClassNumber) o;
+        
+        if (number != that.number) return false;
+        return this.addition.equals(that.addition);
+    }
+    
+    @Override
+    public String toString() {
+        return number + addition;
+    }
+}

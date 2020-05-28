0b53d36d9b3fed97a55b2edb76490ff1b10c5f7a
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-27 21:44:21 -0400

    Created First Version Encryptor

--- src/main/java/lhs/gpa/calculator/backend/CryptoUtils.java
+++ src/main/java/lhs/gpa/calculator/backend/CryptoUtils.java
@@ -5,15 +5,17 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.security.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.time.LocalDate;
 
 public class CryptoUtils {
-    private static final String ALGORITHM = "AES";
-    private static final String TRANSFORMATION = "AES";
-    
     public static OutputStream encryptFile(InputStream toEncrypt, String password)
-            throws NoSuchAlgorithmException, IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException {
+            throws NoSuchAlgorithmException, IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, InvalidKeySpecException, BadPaddingException, IllegalBlockSizeException {
         SecureRandom srandom = SecureRandom.getInstance("SHA1PRNG");
         
         byte[] salt = new byte[8];
@@ -28,46 +30,63 @@ public class CryptoUtils {
         srandom.nextBytes(iv);
         IvParameterSpec ivspec = new IvParameterSpec(iv);
     
-        FileOutputStream out = new FileOutputStream(toEncrypt + ".enc");
+        FileOutputStream out = new FileOutputStream("student_gpa_data_" + LocalDate.now() + ".enc");
         out.write(salt);
         out.write(iv);
     
         Cipher ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
         ci.init(Cipher.ENCRYPT_MODE, skey, ivspec);
-    }
     
-    public static void encrypt(String key, File inputFile, File outputFile) throws CryptoException {
-        doCrypto(Cipher.ENCRYPT_MODE, key, inputFile, outputFile);
-    }
-    
-    public static void decrypt(String key, File inputFile, File outputFile)
-            throws CryptoException {
-        doCrypto(Cipher.DECRYPT_MODE, key, inputFile, outputFile);
-    }
-    
-    private static void doCrypto(int cipherMode, String key, File inputFile,
-                                 File outputFile) throws CryptoException {
-        try {
-            Key    secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
-            Cipher cipher    = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(cipherMode, secretKey);
-            
-            FileInputStream inputStream = new FileInputStream(inputFile);
-            byte[]          inputBytes  = new byte[(int) inputFile.length()];
-            inputStream.read(inputBytes);
-            
-            byte[] outputBytes = cipher.doFinal(inputBytes);
-            
-            FileOutputStream outputStream = new FileOutputStream(outputFile);
-            outputStream.write(outputBytes);
-            
-            inputStream.close();
-            outputStream.close();
-            
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException
-                | InvalidKeyException | BadPaddingException
-                | IllegalBlockSizeException | IOException ex) {
-            throw new CryptoException("Error encrypting/decrypting file", ex);
+        try  {
+            processFile(ci, toEncrypt, out);
         }
+        catch (Exception e) {
+            System.err.println("File Processing Failed");
+            throw e;
+        }
+        
+        return out;
+    }
+    
+    public static InputStream decryptFile(InputStream toDecrypt, String password)
+            throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
+        byte[] salt = new byte[8], iv = new byte[128/8];
+        toDecrypt.read(salt);
+        toDecrypt.read(iv);
+    
+        SecretKeyFactory factory =
+                SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 10000, 128);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKeySpec skey = new SecretKeySpec(tmp.getEncoded(), "AES");
+    
+        Cipher ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        ci.init(Cipher.DECRYPT_MODE, skey, new IvParameterSpec(iv));
+    
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try  {
+            processFile(ci, toDecrypt, out);
+        }
+        catch (Exception e) {
+            System.err.println("File Processing Failed");
+            throw e;
+        }
+        
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+    
+    static private void processFile(Cipher ci,InputStream in,OutputStream out)
+            throws javax.crypto.IllegalBlockSizeException,
+            javax.crypto.BadPaddingException,
+            java.io.IOException
+    {
+        byte[] inputBuffer = new byte[1024];
+        int len;
+        while ((len = in.read(inputBuffer)) != -1) {
+            byte[] outputBuffer = ci.update(inputBuffer, 0, len);
+            if ( outputBuffer != null ) out.write(outputBuffer);
+        }
+        byte[] obuf = ci.doFinal();
+        if ( obuf != null ) out.write(obuf);
     }
 }
